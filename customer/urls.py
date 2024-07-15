4e3175b1d398aa3d5703852ab3a917cb7cc1d64a
= <nozimabonuxamidova2@gmail.com>
2024-07-15 19:47:23 +0500

    first commit

--- customer/urls.py
+++ customer/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from customer.views.auth import login_page, logout_page, register_page
-from customer.views.customers import customers, add_customer, delete_customer, edit_customer, export_data
+# from customer.views.customers import customers, add_customer, delete_customer, edit_customer, export_data
+from customer.views.customers import CustomersListView, CustomersAddListView, CustomerDeleteView, CustomerUpdateView, export_data
 
 urlpatterns = [
-    path('customer-list/', customers, name='customers'),
-    path('add-customer/', add_customer, name='add_customer'),
-    path('customer/<int:pk>/delete', delete_customer, name='delete'),
-    path('customer/<int:pk>/update', edit_customer, name='edit'),
+    path('customer-list/', CustomersListView.as_view(), name='customers'),
+    path('add-customer/', CustomersAddListView.as_view(), name='add_customer'),
+    path('customer/<int:pk>/delete', CustomerDeleteView.as_view(), name='delete'),
+    path('customer/<int:pk>/update', CustomerUpdateView.as_view(), name='edit'),
     # Authentication path
     path('login-page/', login_page, name='login'),
     path('logout-page/', logout_page, name='logout'),
